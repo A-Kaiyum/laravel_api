9bbf14245f8358bc2a801f2815e4adecf0de7d68
Abdul Kaiyum <52959794+A-Kaiyum@users.noreply.github.com>
2022-01-20 17:14:38 +0600

    installation complete and connect database

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// ============= Routes===============
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+Route::get('/students', function () {
+    return "OK";
+}); */
+
+/* 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+}); */
